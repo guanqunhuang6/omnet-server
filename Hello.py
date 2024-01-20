a24115b17c0e34b7b3dac89c72b375fa66d485d0
guanqunhuang6 <41203062+guanqunhuang6@users.noreply.github.com>
2024-01-20 07:59:51 +0000

    update app test

--- Hello.py
+++ Hello.py
@@ -18,33 +18,46 @@ from streamlit.logger import get_logger
 LOGGER = get_logger(__name__)
 
 
+# def run():
+#     st.set_page_config(
+#         page_title="Hello",
+#         page_icon="👋",
+#     )
+
+#     st.write("# Welcome to Streamlit! 👋")
+
+#     st.sidebar.success("Select a demo above.")
+
+#     st.markdown(
+#         """
+#         Streamlit is an open-source app framework built specifically for
+#         Machine Learning and Data Science projects.
+#         **👈 Select a demo from the sidebar** to see some examples
+#         of what Streamlit can do!
+#         ### Want to learn more?
+#         - Check out [streamlit.io](https://streamlit.io)
+#         - Jump into our [documentation](https://docs.streamlit.io)
+#         - Ask a question in our [community
+#           forums](https://discuss.streamlit.io)
+#         ### See more complex demos
+#         - Use a neural net to [analyze the Udacity Self-driving Car Image
+#           Dataset](https://github.com/streamlit/demo-self-driving)
+#         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+#     """
+#     )
+import streamlit as st
+from streamlit_oauth import OAuth2Component
+import os
+import base64
+import json
+
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="omnet",
+        page_icon=":robot:",
     )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    st.title("Google Gmail Example")
+    st.write("This example shows how to use the OAuth2 component to authenticate with Google Gmail API.")
 
 
 if __name__ == "__main__":
