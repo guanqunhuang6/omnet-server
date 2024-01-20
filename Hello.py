070feb39fc11870ce68f99e8a9d4103aa0e3412f
Guanqun Huang <guanqunhuang@Guanquns-Air-M1.attlocal.net>
2024-01-20 00:29:22 -0800

    update secrect security

--- Hello.py
+++ Hello.py
@@ -58,7 +58,41 @@ def run():
     )
     st.title("Google Gmail Example")
     st.write("This example shows how to use the OAuth2 component to authenticate with Google Gmail API.")
+    CLIENT_ID = st.secrets["G_CLIENT_ID"]
+    CLIENT_SECRET = st.secrets["G_CLIENT_SECRET"]
+    AUTHORIZE_ENDPOINT = "https://accounts.google.com/o/oauth2/v2/auth"
+    TOKEN_ENDPOINT = "https://oauth2.googleapis.com/token"
+    REVOKE_ENDPOINT = "https://oauth2.googleapis.com/revoke"
+    
+    if "auth" not in st.session_state:
+        # create a button to start the OAuth2 flow
+        oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_ENDPOINT, TOKEN_ENDPOINT, TOKEN_ENDPOINT, REVOKE_ENDPOINT)
+        result = oauth2.authorize_button(
+            name="Continue with Google",
+            icon="https://www.google.com.tw/favicon.ico",
+            # redirect_uri="http://192.168.1.184:8501",
+            redirect_uri="http://localhost:8501",
+            scope="openid email profile",
+            key="google",
+            extras_params={"prompt": "consent", "access_type": "offline"},
+            use_container_width=True,
+        )
 
+        if result:
+            st.write(result)
+            # decode the id_token jwt and get the user's email address
+            id_token = result["token"]["id_token"]
+            # verify the signature is an optional step for security
+            payload = id_token.split(".")[1]
+            # add padding to the payload if needed
+            payload += "=" * (-len(payload) % 4)
+            payload = json.loads(base64.b64decode(payload))
+            email = payload["email"]
+            st.session_state["auth"] = email
+            st.session_state["token"] = result["token"]
+            st.rerun()
+    else:
+        st.write("You are logged in!")
 
 if __name__ == "__main__":
     run()
