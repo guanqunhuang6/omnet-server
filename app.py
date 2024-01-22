69e8c2c9bb3c7509badeca71d709cd43551021e2
Guanqun Huang <guanqunhuang@Guanquns-MacBook-Air-M1.local>
2024-01-22 00:19:21 -0800

    update cookies

--- app.py
+++ app.py
@@ -1,79 +1,49 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
 from httpx_oauth.oauth2 import BaseOAuth2
 from typing import Any, Dict, List, Optional, Tuple, cast
-
+import pdb
 LOGGER = get_logger(__name__)
 
 
-# def run():
-#     st.set_page_config(
-#         page_title="Hello",
-#         page_icon="👋",
-#     )
-
-#     st.write("# Welcome to Streamlit! 👋")
-
-#     st.sidebar.success("Select a demo above.")
-
-#     st.markdown(
-#         """
-#         Streamlit is an open-source app framework built specifically for
-#         Machine Learning and Data Science projects.
-#         **👈 Select a demo from the sidebar** to see some examples
-#         of what Streamlit can do!
-#         ### Want to learn more?
-#         - Check out [streamlit.io](https://streamlit.io)
-#         - Jump into our [documentation](https://docs.streamlit.io)
-#         - Ask a question in our [community
-#           forums](https://discuss.streamlit.io)
-#         ### See more complex demos
-#         - Use a neural net to [analyze the Udacity Self-driving Car Image
-#           Dataset](https://github.com/streamlit/demo-self-driving)
-#         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-#     """
-#     )
 import streamlit as st
 from streamlit_oauth import OAuth2Component
+import extra_streamlit_components as stx
 import os
 import base64
 import json
 
+from notion_db import NOTION_PRIVATE_API_KEY, NOTION_USER_DATABASE_ID
+from notion_client import Client
+
+notion_client = Client(auth=NOTION_PRIVATE_API_KEY)
+
+st.set_page_config(
+    page_title="omnet",
+    page_icon=":robot:",
+)
+
+
+
+@st.cache_resource(experimental_allow_widgets=True) 
+def get_manager():
+    return stx.CookieManager()
+cookie_manager = get_manager()
+
 def oauth_google():
-    st.set_page_config(
-        page_title="omnet",
-        page_icon=":robot:",
-    )
     st.title("Google Gmail Oauth2")
-    # st.write("This example shows how to use the OAuth2 component to authenticate with Google Gmail API.")
     CLIENT_ID = st.secrets["G_CLIENT_ID"]
     CLIENT_SECRET = st.secrets["G_CLIENT_SECRET"]
     AUTHORIZE_ENDPOINT = "https://accounts.google.com/o/oauth2/v2/auth"
     TOKEN_ENDPOINT = "https://oauth2.googleapis.com/token"
     REVOKE_ENDPOINT = "https://oauth2.googleapis.com/revoke"
     
-    if "auth" not in st.session_state:
+    if not cookie_manager.get(cookie="google_auth") and "google_auth" not in st.session_state:
         # create a button to start the OAuth2 flow
         google_oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_ENDPOINT, TOKEN_ENDPOINT, TOKEN_ENDPOINT, REVOKE_ENDPOINT)
-        googlde_result = google_oauth2.authorize_button(
+        google_result = google_oauth2.authorize_button(
             name="Continue with Google",
             icon="https://www.google.com.tw/favicon.ico",
-            # redirect_uri="http://192.168.1.184:8501",
-            # redirect_uri="http://localhost:8501",
             redirect_uri="https://omnet-server-aw0qwg29mdv.streamlit.app/",
             scope="openid email profile",
             key="google",
@@ -81,18 +51,22 @@ def oauth_google():
             use_container_width=True,
         )
 
-        if googlde_result:
-            st.write(googlde_result)
+        if google_result:
+            st.write(google_result)
             # decode the id_token jwt and get the user's email address
-            id_token = googlde_result["token"]["id_token"]
+            id_token = google_result["token"]["id_token"]
             # verify the signature is an optional step for security
             payload = id_token.split(".")[1]
             # add padding to the payload if needed
             payload += "=" * (-len(payload) % 4)
             payload = json.loads(base64.b64decode(payload))
             email = payload["email"]
-            st.session_state["auth"] = email
-            st.session_state["token"] = googlde_result["token"]
+            st.session_state["google_auth"] = email
+            st.session_state["google_token"] = google_result["token"]
+            # st.set_cookie("google_auth", st.session_state["google_auth"])
+            # st.set_cookie("google_token", st.session_state["google_token"])
+            cookie_manager.set("google_auth", st.session_state["google_auth"])
+            # cookie_manager.set("google_token", st.session_state["google_token"])
             st.rerun()
     else:
         st.write("You are logged in with google!")
@@ -101,7 +75,6 @@ def oauth_notion():
     NOTION_OAUTH2_CLIENT_ID = st.secrets["NOTION_OAUTH2_CLIENT_ID"]
     NOTION_OAUTH2_CLIENT_SECRET = st.secrets["NOTION_OAUTH2_CLIENT_SECRET"]
     NOTION_OAUTH2_AUTHORIZATION_URL = "https://api.notion.com/v1/oauth/authorize"
-    # NOTION_OAUTH2_AUTHORIZATION_URL = "https://api.notion.com/v1/oauth/authorize?client_id=6c404a6b-7239-4c4d-b0b0-419109232c69&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fomnet.life%2Fnotion%2Fcallback"
     NOTION_OAUTH2_TOKEN_URL = "https://api.notion.com/v1/oauth/token"
 
 
@@ -167,7 +140,6 @@ def oauth_notion():
         notion_result = notion_oauth2.authorize_button(
             name="Login with Notion",
             icon="https://www.notion.so/images/favicon.ico",
-            # redirect_uri="http://localhost:8501",
             redirect_uri="https://omnet-server-aw0qwg29mdv.streamlit.app/",
             scope="user",
             key="notion",
@@ -184,6 +156,9 @@ def oauth_notion():
         # st.button("Logout")
         # del st.session_state["notion_token"]
         
+def import_gmail():
+    pass
+
 if __name__ == "__main__":
     oauth_google()
     oauth_notion()
---
+++ notion_db.py
@@ -0,0 +1,8 @@
+import os
+# run this command on the terminal: export NOTION_INTEGRATION_API=secret
+NOTION_PRIVATE_API_KEY = os.environ.get('NOTION_INTEGRATION_API')
+
+NOTION_USER_DATABASE_ID = "34b4bb1462bb4852841793f47761eb96"
+
+
+    
